53e7ab61f4690643e4c651d6ffd34a8f40d5efc6
MarwaMagdi28 <moro.magdi@gmail.com>
2024-09-30 22:03:34 +0300

    yaraab

--- main.tf
+++ main.tf
@@ -2,25 +2,9 @@ provider "aws" {
   region = "us-west-1"
 }
 
-variable "aws_public_key_name" {
-  default = "prometheus_aws_rsa"
-}
-
-resource "tls_private_key" "sskeygen_execution" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "prometheus_key_pair" {
-  depends_on = [tls_private_key.sskeygen_execution]
-  key_name   = "${var.aws_public_key_name}"
-  public_key = "${tls_private_key.sskeygen_execution.public_key_openssh}"
-}
-
 resource "aws_instance" "ec2_instance" {
   ami           = "ami-047d7c33f6e7b4bc4" # Ubuntu 18.04 AMI
   instance_type = "t2.micro"
-  key_name      = "${aws_key_pair.prometheus_key_pair.id}"
   associate_public_ip_address = true
 
   tags = {
@@ -35,12 +19,11 @@ resource "aws_instance" "ec2_instance" {
             sudo systemctl enable docker
             sudo docker run -d --name prometheus -p 9090:9090 prom/prometheus
             sudo docker run -d --name grafana -p 3000:3000 grafana/grafana
-            ssh-keygen -t rsa -f ~/.ssh/my_ssh_key -N "" < /dev/null
-            echo "$(cat ~/.ssh/my_ssh_key.pub)" >> ~/.ssh/authorized_keys
+            echo ${{ secrets.SSH_PUBLIC_KEY }} >> ~/.ssh/authorized_keys
             EOF
 
     provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -u ec2-user -h ${self.public_ip} --private-key ${aws_key_pair.prometheus_key_pair.id}.pem ./playbook.yml"
+    command = "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -u ec2-user -h ${self.public_ip} --private-key ${{ secrets.SSH_PRIVATE_KEY }} ./playbook.yml"
   }
 }
 
