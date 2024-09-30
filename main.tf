7535c3f18ef5d71c011d955cff72281151af9f89
MarwaMagdi28 <moro.magdi@gmail.com>
2024-09-30 13:13:46 +0300

    1

--- main.tf
+++ main.tf
@@ -2,33 +2,13 @@ provider "aws" {
   region = "us-west-1"
 }
 
-variable "aws_public_key_name" {
-  default = "prometheus_aws_rsa"
-}
-
-resource "tls_private_key" "sskeygen_execution" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "prometheus_key_pair" {
-  depends_on = ["tls_private_key.sskeygen_execution"]
-  key_name   = "${var.aws_public_key_name}"
-  public_key = "${tls_private_key.sskeygen_execution.public_key_openssh}"
-}
-
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "ec2_instance" {
   ami           = "ami-047d7c33f6e7b4bc4" # Ubuntu 18.04 AMI
   instance_type = "t2.micro"
   associate_public_ip_address = true
-   key_name   = "${aws_key_pair.prometheus_key_pair.id}"
-   connection {
-    user        = "ec2-user"
-    host = self.public_ip
-    private_key = "${tls_private_key.sskeygen_execution.private_key_pem}"
-  }
+
   tags = {
-    Name = "ubuntu"
+    Name = "ec2_instance"
   }
 
   user_data = <<-EOF
@@ -42,10 +22,10 @@ resource "aws_instance" "ubuntu" {
             EOF
 
     provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -u ec2-user -i '${self.public_ip},' --private-key ${tls_private_key.sskeygen_execution.private_key_pem} ./playbook.yml"
+    command = "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -u ec2-user -i '${self.public_ip},' --private-key ${{ secrets.SSH_PRIVATE_KEY }} ./playbook.yml"
   }
 }
 
 output "instance_ip" {
-  value = aws_instance.ubuntu.public_ip
+  value = aws_instance.ec2_instance.public_ip
 }
