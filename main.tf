7efb555f22c3f96ddb7aee4a48156d687e587414
MarwaMagdi28 <moro.magdi@gmail.com>
2024-09-30 09:37:57 +0300

    1

--- main.tf
+++ main.tf
@@ -1,8 +1,26 @@
 provider "aws" {
-  region     = "us-west-1"
+  region = "us-west-1"
 }
 
-resource "aws_instance" "my_instance" {
-  ami           = "ami-047d7c33f6e7b4bc4"  # Replace with your desired AMI ID
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-005fc0f236362e99f" # Ubuntu 18.04 AMI
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "ubuntu"
+  }
+
+  user_data = <<-EOF
+            #!/bin/bash
+            sudo apt-get update -y
+            sudo apt-get install -y docker.io
+            sudo systemctl start docker
+            sudo systemctl enable docker
+            sudo docker run -d --name prometheus -p 9090:9090 prom/prometheus
+            sudo docker run -d --name grafana -p 3000:3000 grafana/grafana
+            EOF
+}
+
+output "instance_ip" {
+  value = aws_instance.ubuntu.public_ip
 }
