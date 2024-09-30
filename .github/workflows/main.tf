88307e10f2becee27b445f72a024317356faacda
MarwaMagdi28 <moro.magdi@gmail.com>
2024-09-30 08:43:13 +0300

    11

---
+++ .github/workflows/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 18.04 AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my_instance"
+  }
+
+  user_data = <<-EOF
+            #!/bin/bash
+            sudo apt-get update -y
+            sudo apt-get install -y docker.io
+            sudo systemctl start docker
+            sudo systemctl enable docker
+            sudo docker run -d --name prometheus -p 9090:9090 prom/prometheus
+            sudo docker run -d --name grafana -p 3000:3000 grafana/grafana
+            EOF
+}
+
+output "instance_ip" {
+  value = aws_instance.my_instance.public_ip
+}
