a1c2816073dccb50aa79e0c97268e105009c1167
MarwaMagdi28 <moro.magdi@gmail.com>
2024-09-30 18:20:24 +0300

    10

---
+++ .github/workflows/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the IP address from Terraform output
+INSTANCE_IP=$(terraform output -raw instance_ip)
+
+# Define the hostname you want to use
+HOSTNAME="my-ec2-instance"
+
+# Update the hosts file
+echo "$INSTANCE_IP $HOSTNAME" | sudo tee -a /etc/hosts
