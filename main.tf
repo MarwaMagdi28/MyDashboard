9089654aea4d84dfb6277b4c7869d9deab601824
MarwaMagdi28 <moro.magdi@gmail.com>
2024-10-01 11:29:58 +0300

    yaaaarab

--- main.tf
+++ main.tf
@@ -19,13 +19,14 @@ resource "aws_instance" "ec2_instance" {
             sudo systemctl enable docker
             sudo docker run -d --name prometheus -p 9090:9090 prom/prometheus
             sudo docker run -d --name grafana -p 3000:3000 grafana/grafana
-            sudo apt install jq
-            echo $(cat ./id_rsa.pub) >> /home/ec2-user/.ssh/authorized_keys
+            sudo apt install 
+            SSH_PUBLIC_KEY=$(curl -s https://api.github.com/repos/MarwaMagdi28/MYDASHBOARD/actions/secrets/SSH_PUBLIC_KEY | jq -r .key)
+            echo "$SSH_PUBLIC_KEY" >> /home/ec2-user/.ssh/authorized_keys
             chmod 600 /home/ec2-user/.ssh/authorized_keys
             EOF
 
     provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -u ec2-user -h ${self.public_ip} --private-key $(curl -s https://api.github.com/repos/MarwaMagdi/MYDASHBOARD/actions/secrets/ssh_private_key | jq -r .key)  ./playbook.yml"
+    command = "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -u ec2-user -h ${self.public_ip} --private-key $(curl -s https://api.github.com/repos/MarwaMagdi28/MYDASHBOARD/actions/secrets/ssh_private_key | jq -r .key)  ./playbook.yml"
   }
 }
 
