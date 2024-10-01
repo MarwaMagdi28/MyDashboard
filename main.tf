87d944da8c75510011c1b7943ba5f95bcfce3a65
MarwaMagdi28 <moro.magdi@gmail.com>
2024-10-01 10:22:35 +0300

    yarab

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "ec2_instance" {
             sudo systemctl enable docker
             sudo docker run -d --name prometheus -p 9090:9090 prom/prometheus
             sudo docker run -d --name grafana -p 3000:3000 grafana/grafana
-            echo $(curl -s https://api.github.com/repos/MarwaMagdi/MYDASHBOARD/actions/secrets/ssh_public_key | jq -r .key) >> ~/.ssh/authorized_keys
+            echo "yarab" >> ~/.ssh/authorized_keys
             EOF
 
     provisioner "local-exec" {
