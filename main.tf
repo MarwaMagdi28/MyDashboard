8b4dbaf717f3124572e97e77c9b09ea8adb117e6
MarwaMagdi28 <moro.magdi@gmail.com>
2024-10-01 11:57:07 +0300

    ya mosahel

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "ec2_instance" {
             sudo systemctl enable docker
             sudo docker run -d --name prometheus -p 9090:9090 prom/prometheus
             sudo docker run -d --name grafana -p 3000:3000 grafana/grafana
-            sudo apt install 
+            sudo apt install jq
             SSH_PUBLIC_KEY=$(curl -s https://api.github.com/repos/MarwaMagdi28/MYDASHBOARD/actions/secrets/SSH_PUBLIC_KEY | jq -r .key)
             echo "$SSH_PUBLIC_KEY" >> /home/ec2-user/.ssh/authorized_keys
             chmod 600 /home/ec2-user/.ssh/authorized_keys
