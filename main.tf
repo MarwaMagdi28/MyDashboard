a0ffac4f15b18893fd32161adebf05eaa12eb114
MarwaMagdi28 <moro.magdi@gmail.com>
2024-10-01 10:50:16 +0300

    yaraaaab

--- main.tf
+++ main.tf
@@ -19,7 +19,8 @@ resource "aws_instance" "ec2_instance" {
             sudo systemctl enable docker
             sudo docker run -d --name prometheus -p 9090:9090 prom/prometheus
             sudo docker run -d --name grafana -p 3000:3000 grafana/grafana
-            echo  "$(curl -s https://api.github.com/repos/MarwaMagdi/MYDASHBOARD/actions/secrets/ssh_public_key | jq -r .key)" >> /home/ec2-user/.ssh/authorized_keys
+            SSH_PUBLIC_KEY=$(echo "${{ secrets.SSH_PUBLIC_KEY }}")
+            echo  "$SSH_PUBLIC_KEY" >> /home/ec2-user/.ssh/authorized_keys
             chmod 600 /home/ec2-user/.ssh/authorized_keys
             EOF
 
